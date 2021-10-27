64513633f3bec7ac16776bea465cd293fca3d53a
YeniValle <yenivalle@YeniValles-MBP.attlocal.net>
2021-10-26 22:52:30 -0400

    Add cards deck

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
 let message = "";
-let firstCard = 10;
-let secondCard = 11;
+let firstCard = randomCard();
+let secondCard = randomCard();
+let cards = [firstCard, secondCard];
+//let cardsDeck = cards.fill(Math.floor(Math.random() * 10) + 1);
 let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isDead = false;
+let messageEl = document.getElementById("message");
+let sumEl = document.getElementById("result");
+let cardEl = document.getElementById("card");
 
-function startGame() {
-    if (sum <= 20) {
-        message = "Do you need a new card?"
-    } else if(sum === 21) {
-        message = "Blackjack"
-    }else{
-        message = "You out of the game"
+function startGame(){
+    renderGame()
+}
+
+function renderGame() {
+    cardEl.textContent = "Cards: ";
+    for (let i = 0; i < cards.length; i++) {
+        cardEl.textContent += `${cards[i]} `;
     }
-    console.log(message)
+    sumEl.textContent = `Sum: ${sum}`;
+    if (sum <= 20) {
+        message = "Do you want a new card?";
+    } else if(sum === 21) {
+        message = "You've got Blackjack!";
+        hasBlackJack = true
+    }else{
+        message = "Sorry, you're out of the game!"
+        isDead = true
+    }
+    messageEl.textContent = message;
+
+}
+
+function randomCard(){
+    return Math.floor(Math.random() * 10) + 2
+}
+
+function newCard(){
+    let otherCard = randomCard();
+    sum += otherCard;
+    cards.push(otherCard);
+    renderGame()
 }
